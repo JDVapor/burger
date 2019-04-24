ed574fd45ab288add3190eb580f074bb6075b820
JDVapor <jdvapordestiny@gmail.com>
2019-04-23 21:37:28 -0700

    add code to files for testing

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,45 @@
 var connection = require("./connection.js");
 
 var orm = {
-selectAll: function(){},
 
-insertOne: function(){},
+  selectAll: tableInput => {
+    return new Promise((resolve, reject) => {
+      var queryString = "SELECT * FROM ??;";
+      connection.query(queryString, [tableInput], (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  },
 
-updateOne: function(){}
+  insertOne: (tableInput, colOneName, colTwoName, burgerName, devour) => {
+    return new Promise((resolve, reject) => {
+      var queryString = "INSERT INTO ??(??, ??) VALUES(??, ??);";
+      connection.query(queryString, [tableInput, colOneName, colTwoName, burgerName, devour], (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  },
+
+  updateOne: (tableInput, colOneName, colTwoName, burgerName, devour, condition) => {
+    return new Promise((resolve, reject) => {
+      var queryString = "UPDATE ?? SET ?? = ??, ?? = ?? WHERE id = ??";
+      connection.query(queryString, [tableInput, colOneName, burgerName, colTwoName, devour, condition], (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
 
 };
 
--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,12 @@
-var orm = require("./../config/orm.js");
+var orm = require("../config/orm.js");
+
+var burger = {
+  selectAll: orm.selectAll("burgers"),
+
+  insertOne: orm.insertOne("burgers", "burger_name", "devoured", burgerName, devour),
+
+  updateOne: orm.updateOne("burgers", "burger_name", "devoured", burgerName, devour, id)
+
+};
+
+module.exports = burger;
